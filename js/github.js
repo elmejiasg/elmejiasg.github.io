b5d57fa9bb1d5cd4efd377590c33ddc4dfea5620
Elena Mejías <elmejiasg@gmail.com>
2025-06-28 12:08:18 +0200

    banners2

--- js/github.js
+++ js/github.js
@@ -61,6 +61,8 @@ const defaultBanners = [
   "assets/banners/banner1.png",
   "assets/banners/banner2.png",
   "assets/banners/banner3.png",
+  "assets/banners/banner4.png",
+  "assets/banners/banner5.png"
 ];
 
 export async function renderProjects(repos, lang = "es") {
